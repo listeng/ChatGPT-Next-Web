1f584bc8f2afc15faeb00a82ce9eaf9628e0dd2c
tls <1536813+listeng@users.noreply.github.com>
2024-11-18 09:05:00 +0800

    fix locale

--- app/locales/jp.ts
+++ app/locales/jp.ts
@@ -1,21 +1,14 @@
 import { SubmitKey } from "../store/config";
 import type { PartialLocaleType } from "./index";
 import { getClientConfig } from "../config/client";
-import { SAAS_CHAT_UTM_URL } from "@/app/constant";
 const isApp = !!getClientConfig()?.isApp;
 
 const jp: PartialLocaleType = {
   WIP: "この機能は開発中です",
   Error: {
     Unauthorized: isApp
-      ? `😆 会話中に問題が発生しましたが、心配しないでください:
-    \\ 1️⃣ 設定なしで始めたい場合は、[ここをクリックしてすぐにチャットを開始 🚀](${SAAS_CHAT_UTM_URL})
-    \\ 2️⃣ 自分のOpenAIリソースを使用したい場合は、[ここをクリックして](/#/settings)設定を変更してください ⚙️`
-      : `😆 会話中に問題が発生しましたが、心配しないでください:
-    \ 1️⃣ 設定なしで始めたい場合は、[ここをクリックしてすぐにチャットを開始 🚀](${SAAS_CHAT_UTM_URL})
-    \ 2️⃣ プライベートデプロイ版を使用している場合は、[ここをクリックして](/#/auth)アクセストークンを入力してください 🔑
-    \ 3️⃣ 自分のOpenAIリソースを使用したい場合は、[ここをクリックして](/#/settings)設定を変更してください ⚙️
- `,
+      ? "無効なAPIキーと検出されました、[設定ページ](/#/settings)でAPIキーが正しく設定されているか確認してください。"
+      : "ユーザーはログインしていません、[ログイン](/#/auth)へ行ってログインしてください。",
   },
   Auth: {
     Title: "パスワードが必要です",
